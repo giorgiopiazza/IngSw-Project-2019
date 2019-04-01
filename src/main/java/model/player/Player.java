cb3ce48de276bb8664d24c5c8b86733547ab9a86
Megapiro <framegapiro@gmail.com>
2019-04-01 12:02:36 +0200

    Added Player and PlayerPosition classes

--- src/main/java/model/player/Player.java
+++ src/main/java/model/player/Player.java
@@ -1,4 +1,49 @@
 package model.player;
 
-public class Player {
+import enumerations.Color;
+
+public abstract class Player {
+    private final String nickname;
+    private final Color color;
+    private final boolean firstPlayer;
+    private final PlayerBoard playerBoard;
+
+    private PlayerPosition position;
+    private int points;
+
+    public Player (String nickname, Color color, boolean firstPlayer, PlayerPosition position, PlayerBoard playerBoard) {
+
+        this.nickname = nickname;
+        this.color = color;
+        this.firstPlayer = firstPlayer;
+        this.position = position;
+        this.playerBoard = playerBoard;
+
+        points = 0;
+    }
+
+    public String getNickname() { return nickname; }
+    public Color getColor () { return color; }
+    public PlayerPosition getPosition () { return position; }
+    public int getPoints () { return points; }
+
+    public boolean isFirstPlayer () {
+        return firstPlayer;
+    }
+
+    public void changePosition(int newX, int newY) throws IndexOutOfBoundsException {
+
+        if ((newX < 0) || (newX > 4)) {
+            throw new IndexOutOfBoundsException("The X you wanted to change is not in the map");
+        }
+        if ((newY < 0) || (newY > 5)) {
+            throw new IndexOutOfBoundsException("The Y you wanted to change is not in the map");
+        }
+        this.position.setCoordX(newX);
+        this.position.setCoordY(newY);
+    }
+
+    public void addPoints (int pointsGained) {
+        points = this.points + pointsGained;
+    }
 }
---
+++ src/main/java/model/player/PlayerPosition.java
@@ -0,0 +1,19 @@
+package model.player;
+
+public class PlayerPosition {
+
+    private int coordX;
+    private int coordY;
+
+    public PlayerPosition (int coordX, int coordY) {
+        this.coordX = coordX;
+        this.coordY = coordY;
+    }
+
+    public int getCoordX () { return coordX; }
+    public void setCoordX (int coordX) { this.coordX = coordX; }
+
+    public int getCoordY () { return coordY; }
+    public void setCoordY (int coordY) { this.coordY = coordY; }
+
+}
