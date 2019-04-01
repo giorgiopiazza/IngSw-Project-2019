4e1896f5f914ebe902358d7031fda5c5e555caee
tosettil <lorenzo.tosetti@mail.polimi.it>
2019-04-01 02:27:26 +0200

    Add AdrenalinaException constructors

--- src/main/java/exceptions/AdrenalinaException.java
+++ src/main/java/exceptions/AdrenalinaException.java
@@ -1,4 +1,21 @@
 package exceptions;
 
 public abstract class AdrenalinaException extends Exception {
+    public AdrenalinaException() { super(); }
+
+    public AdrenalinaException(String message) {
+        super(message);
+    }
+
+    public AdrenalinaException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public AdrenalinaException(Throwable cause) {
+        super(cause);
+    }
+
+    public AdrenalinaException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
 }
