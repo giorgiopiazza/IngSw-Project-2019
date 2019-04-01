6d093a4c4151a00e7bcf3b6709e72f78df250555
tosettil <lorenzo.tosetti@mail.polimi.it>
2019-04-01 02:01:22 +0200

    Create Map class

---
+++ src/main/java/model/map/Map.java
@@ -0,0 +1,13 @@
+package model.map;
+
+public class Map {
+    public static final int MAX_ROWS = 3;
+    public static final int MAX_COLUMNS = 4;
+    private Square rooms[][];
+
+    public Map() {
+        rooms = new Square[MAX_ROWS][MAX_COLUMNS];
+    }
+
+
+}
