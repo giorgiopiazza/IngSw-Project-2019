768345c4e5006974a396836b4dd91a700e74d62a
tosettil <lorenzo.tosetti@mail.polimi.it>
2019-04-04 17:28:43 +0200

    End GameTest

--- src/test/java/model/game/GameTest.java
+++ src/test/java/model/game/GameTest.java
@@ -1,11 +1,10 @@
 package model.game;
 
+import enumerations.Color;
 import exceptions.AdrenalinaException;
-import exceptions.game.GameAlreadyStartedException;
-import exceptions.game.MaxPlayerException;
-import exceptions.game.NotEnoughPlayersException;
+import exceptions.game.*;
 import model.Game;
-import model.player.Player;
+import model.player.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -33,7 +32,14 @@ class GameTest {
         instance.addPlayer(mock(Player.class));
         instance.addPlayer(mock(Player.class));
         instance.addPlayer(mock(Player.class));
+
+        assertNotNull(instance.setTerminator(true));
+
         instance.addPlayer(mock(Player.class));
+
+        assertThrows(MaxPlayerException.class, () -> instance.addPlayer(mock(Player.class)));
+
+        assertNull(instance.setTerminator(false));
         instance.addPlayer(mock(Player.class));
 
         assertEquals(5, instance.playersNumber());
@@ -44,6 +50,10 @@ class GameTest {
         instance.startGame();
 
         assertThrows(GameAlreadyStartedException.class, () -> instance.addPlayer(null));
+
+        instance.stopGame();
+
+        instance.flush();
     }
 
     @Test
@@ -57,6 +67,110 @@ class GameTest {
 
         assertTrue(instance.isStarted());
         assertThrows(GameAlreadyStartedException.class, instance::startGame);
+
+        instance.stopGame();
+
+        instance.flush();
     }
 
+    @Test
+    void stopGame() throws AdrenalinaException {
+        assertThrows(GameAlreadyStartedException.class, instance::stopGame);
+
+        instance.addPlayer(mock(Player.class));
+        instance.addPlayer(mock(Player.class));
+        instance.addPlayer(mock(Player.class));
+
+        instance.startGame();
+        assertTrue(instance.isStarted());
+
+        instance.stopGame();
+        assertFalse(instance.isStarted());
+
+        instance.flush();
+    }
+
+    @Test
+    void various() throws AdrenalinaException {
+        instance.addPlayer(mock(Player.class));
+        instance.addPlayer(mock(Player.class));
+        instance.addPlayer(mock(Player.class));
+
+        assertThrows(MaximumKillshotExceededException.class, () -> instance.setKillShotNum(9));
+        instance.setKillShotNum(3);
+        assertEquals(3, instance.getKillShotNum());
+
+        instance.startGame();
+
+        assertThrows(GameAlreadyStartedException.class, instance::flush);
+        assertThrows(GameAlreadyStartedException.class, instance::flushKillshot);
+
+        instance.addKillShot(mock(KillShot.class));
+        assertEquals(2, instance.remainingSkulls());
+
+        instance.addKillShot(mock(KillShot.class));
+        instance.addKillShot(mock(KillShot.class));
+
+        assertThrows(NullPointerException.class, () -> instance.addKillShot(null));
+        assertThrows(KillShotsTerminatedException.class, () -> instance.addKillShot(mock(KillShot.class)));
+        assertThrows(GameAlreadyStartedException.class, () -> instance.setKillShotNum(4));
+
+        instance.stopGame();
+        instance.flush();
+    }
+
+    @Test
+    void terminator() throws AdrenalinaException {
+        instance.addPlayer(mock(Player.class));
+        instance.addPlayer(mock(Player.class));
+        instance.addPlayer(mock(Player.class));
+        instance.addPlayer(mock(Player.class));
+        instance.addPlayer(mock(Player.class));
+
+        assertThrows(MaxPlayerException.class, () -> instance.setTerminator(true));
+        assertThrows(TerminatorNotPresentException.class, instance::getTerminator);
+
+        instance.startGame();
+
+        assertThrows(GameAlreadyStartedException.class, () -> instance.setTerminator(true));
+
+        instance.stopGame();
+        instance.flush();
+
+        Player player = new UserPlayer("tose", Color.YELLOW, true, new PlayerBoard(), false);
+        instance.addPlayer(player);
+        instance.addPlayer(mock(Player.class));
+        instance.addPlayer(mock(Player.class));
+
+        assertNull(instance.setTerminator(false));
+
+        Player terminator = instance.setTerminator(true);
+        assertNotNull(terminator);
+        assertEquals(terminator, instance.getTerminator());
+
+        instance.flush();
+    }
+
+    @Test
+    void spawnPlayer() throws AdrenalinaException {
+        Player player = new UserPlayer("tose", Color.YELLOW, true, new PlayerBoard(), false);
+        Player notContained = new UserPlayer("gio", Color.YELLOW, true, new PlayerBoard(), false);
+
+        instance.addPlayer(player);
+        instance.addPlayer(mock(Player.class));
+        instance.addPlayer(mock(Player.class));
+
+        assertThrows(GameAlreadyStartedException.class, () -> instance.spawnPlayer(player, mock(PlayerPosition.class)));
+
+        instance.startGame();
+
+        assertThrows(NullPointerException.class, () -> instance.spawnPlayer(null, mock(PlayerPosition.class)));
+        assertThrows(NullPointerException.class, () -> instance.spawnPlayer(player, null));
+        assertThrows(UnknownPlayerException.class, () -> instance.spawnPlayer(notContained, mock(PlayerPosition.class)));
+
+        instance.spawnPlayer(player, mock(PlayerPosition.class));
+
+        instance.stopGame();
+        instance.flush();
+    }
 }
