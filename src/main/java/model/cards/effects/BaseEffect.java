d2b7c37542d653566f79bd1073d9ac6080b834ba
Megapiro <framegapiro@gmail.com>
2019-04-21 22:25:55 +0200

    UML update with code related TODO

--- src/main/java/model/cards/effects/BaseEffect.java
+++ src/main/java/model/cards/effects/BaseEffect.java
@@ -3,6 +3,8 @@ package model.cards.effects;
 import model.player.AmmoQuantity;
 
 public class BaseEffect extends Effect {
+    // TODO we can add a description of the effect to give a better understanding of the weapon while playing with CLI
+
     public BaseEffect(AmmoQuantity cost) {
         setCost(cost);
     }
