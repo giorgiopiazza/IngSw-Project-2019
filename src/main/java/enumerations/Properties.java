43abacf5092cc8ad866a2a6e1c3502c2a59f238b
Megapiro <framegapiro@gmail.com>
2019-04-26 20:24:57 +0200

    Add moveInLine attribute to Properties enum and weapons.json

--- src/main/java/enumerations/Properties.java
+++ src/main/java/enumerations/Properties.java
@@ -3,7 +3,7 @@ package enumerations;
 public enum Properties {
     VISIBLE("visible"), CONCATENATED_VISIBLE("concatenatedVisible"), INLINE("inLine"),
     DISTANCE("distance"), MIN_DISTANCE("minDistance"), SAME_POSITION("samePosition"),
-    TARGET_NUM("targetNum"), MAX_TARGET_NUM("maxTargetNum"),
+    TARGET_NUM("targetNum"), MAX_TARGET_NUM("maxTargetNum"), MOVE_INLINE("moveInLine"),
     MOVE("move"), MOVE_TARGET("moveTarget"), MAX_MOVE_TARGET("maxMoveTarget"),
     MOVE_TARGET_BEFORE("moveTargetBefore"), MOVE_TO_LAST_TARGET("moveToLastTarget"),
     DAMAGE_DISTRIBUTION("damageDistribution"), MARK_DISTRIBUTION("markDistribution");
