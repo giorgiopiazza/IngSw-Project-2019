32ef7ae74cb104e95bd54afca72d87761d6952bb
Giorgio Piazza <gio.piazza97@gmail.com>
2019-05-03 08:34:26 +0200

    Add turn manager

---
+++ src/main/java/controller/TurnManager.java
@@ -0,0 +1,36 @@
+package controller;
+
+import model.player.UserPlayer;
+
+import java.util.List;
+
+public class TurnManager {
+    private UserPlayer turnOwner;
+
+    private List<UserPlayer> players;
+    private List<UserPlayer> deathPlayers;
+
+    private int count;
+
+    public TurnManager(List<UserPlayer> players) {
+        this.players = players;
+    }
+
+    public UserPlayer getTurnOwner() {
+        return turnOwner;
+    }
+
+    public void addDeathPlayer(UserPlayer player) {
+        deathPlayers.add(player);
+    }
+
+    public void nextTurn() {
+        if (!deathPlayers.isEmpty()) {
+            turnOwner = deathPlayers.remove(deathPlayers.size() - 1);
+        } else {
+            count++;
+            count = count % players.size();
+            turnOwner = players.get(count);
+        }
+    }
+}
--- src/main/java/enumerations/PossibleAction.java
+++ src/main/java/enumerations/PossibleAction.java
@@ -1,7 +1,7 @@
 package enumerations;
 
 public enum PossibleAction {
-    PICK_TWO_POWERUPS, SPAWN_TERMINATOR, CHOOSE_SPAWN, CHOOSE_RESPAWN, // possible actions a player must do even before the waiting to play state
+    SPAWN_TERMINATOR, CHOOSE_SPAWN, CHOOSE_RESPAWN,                    // possible actions a player must do even before the waiting to play state
     MOVE, MOVE_AND_PICK, SHOOT, RELOAD,                                // possible actions a player can do if he has no ADRENALINE
     ADRENALINE_PICK, ADRENALINE_SHOOT,                                 // possible adrenaline actions
     FRENZY_MOVE, FRENZY_PICK, FRENZY_SHOOT,                            // possible x2 final frenzy actions
