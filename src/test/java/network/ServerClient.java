78c096f1f0d05031325ae2e61f66625a6f9c7c29
tosettil-polimi <lorenzo.tosetti@mail.polimi.it>
2019-05-12 18:31:10 +0200

    Add send object to ServerThread, object receive to Client

--- src/test/java/network/ServerClient.java
+++ src/test/java/network/ServerClient.java
@@ -1,13 +1,10 @@
 package network;
 
+import model.GameSerialized;
 import network.client.Client;
 import network.server.MultiServer;
 import org.junit.jupiter.api.Test;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.ServerSocket;
 import java.util.Random;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -26,11 +23,32 @@ public class ServerClient {
     }
 
     @Test
-    void clientTest() throws IOException {
+    void clientTest() throws InterruptedException {
         Client client = new Client("localhost");
 
-        client.sendMessage(new Random().nextInt() % 100 + "");
-        Logger.getGlobal().log(Level.INFO,client.receiveMessage());
+        while (true) {
+            int n = new Random().nextInt() % 100;
+            client.sendMessage("gameState");
+            GameSerialized gameSerialized = (GameSerialized) client.receiveObject();
+            Logger.getGlobal().log(Level.INFO, gameSerialized.toString());
+            if (n % 11 == 0) break;
+            Thread.sleep(5000);
+        }
+
+        client.close();
+    }
+
+    @Test
+    void clientTest2() throws InterruptedException {
+        Client client = new Client("localhost");
+
+        while (true) {
+            int n = new Random().nextInt() % 100;
+            client.sendMessage(n + "");
+            Logger.getGlobal().log(Level.INFO, client.receiveMessage());
+            if (n % 11 == 0) break;
+            Thread.sleep(5000);
+        }
 
         client.close();
     }
