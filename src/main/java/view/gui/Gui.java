378e924aa18084a837d35a751402d80d46db9551
Giorgio Piazza <gio.piazza97@gmail.com>
2019-05-30 16:48:42 +0200

    Update Connection scene

--- src/main/java/view/gui/Gui.java
+++ src/main/java/view/gui/Gui.java
@@ -1,14 +1,16 @@
 package view.gui;
 
 import javafx.application.Application;
+import javafx.css.PseudoClass;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.TextField;
+import javafx.scene.control.TextFormatter;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCombination;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
-
 import java.io.IOException;
 import java.util.logging.Logger;
 
@@ -23,18 +25,21 @@ public class Gui extends Application {
         window.setFullScreenExitHint("");
         window.setFullScreenExitKeyCombination(KeyCombination.NO_MATCH);
 
-        setMenuLayout();
+        window.setScene(new Scene(new Pane()));
+
+        setMainMenuLayout();
 
         window.show();
     }
 
-    private void setMenuLayout() {
+    private void setMainMenuLayout() {
         FXMLLoader loader = new FXMLLoader(Gui.class.getClassLoader().getResource("fxml/menuScene.fxml"));
 
         try {
-            Scene scene = new Scene(loader.load());
+            Pane pane = loader.load();
+            Scene scene = window.getScene();
+            scene.setRoot(pane);
 
-            window.setScene(scene);
             ImageView exitButton = (ImageView) scene.lookup("#exitButton");
             exitButton.addEventHandler(MouseEvent.MOUSE_CLICKED, event -> System.exit(0));
 
@@ -50,8 +55,23 @@ public class Gui extends Application {
 
         try {
             Pane pane = loader.load();
-            window.getScene().setRoot(pane);
+            Scene scene = window.getScene();
+            scene.setRoot(pane);
 
+            ImageView backButton = (ImageView) scene.lookup("#backButton");
+            backButton.addEventHandler(MouseEvent.MOUSE_CLICKED, event -> setMainMenuLayout());
+
+            TextField portField = (TextField) scene.lookup("#portField");
+            portField.setTextFormatter(new TextFormatter<String>(change -> {
+                String input = change.getText();
+                if (input.matches("[0-9]*")) {
+                    return change;
+                }
+
+                return null;
+            }));
+
+            //usernameField.pseudoClassStateChanged(PseudoClass.getPseudoClass("error"), true);
         } catch (IOException e) {
             Logger.getGlobal().severe(e.getMessage());
         }
