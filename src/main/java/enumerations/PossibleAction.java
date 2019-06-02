a4ad64b4b4d102bc202215ad43f0c221907ae06e
tosettil <lorenzo.tosetti@mail.polimi.it>
2019-06-02 17:34:02 +0200

    Create in cli ClientRoundManager management, to finish

--- src/main/java/enumerations/PossibleAction.java
+++ src/main/java/enumerations/PossibleAction.java
@@ -6,5 +6,32 @@ public enum PossibleAction {
     ADRENALINE_PICK, ADRENALINE_SHOOT,                                 // possible adrenaline actions
     FRENZY_MOVE, FRENZY_PICK, FRENZY_SHOOT,                            // possible x2 final frenzy actions
     LIGHT_FRENZY_PICK, LIGHT_FRENZY_SHOOT,                             // possible x1 final frenzy actions
-    TERMINATOR_ACTION                                                  // action that everybody has if the terminator is present in the game
+    TERMINATOR_ACTION;                                                 // action that everybody has if the terminator is present in the game
+
+    @Override
+    public String toString() {
+        switch (this) {
+            case SPAWN_TERMINATOR:
+                return "Spawn terminator";
+            case CHOOSE_SPAWN:
+                return "Choose spawn point";
+            case CHOOSE_RESPAWN:
+                return "Choose respawn point";
+            case MOVE:
+                return "Move up to 3 boxes";
+            case MOVE_AND_PICK:
+                return "Move up to 1 box and pick up weapon or power up";
+            case SHOOT:
+                return "Shoot a player";
+            case RELOAD:
+                return "Reload your weapons";
+            case ADRENALINE_PICK:
+                return "Move up to 2 boxes and pick up weapon or power up";
+            case ADRENALINE_SHOOT:
+                return "Move up to 1 box and shoot a player";
+
+            default:
+                return this + "";
+        }
+    }
 }
