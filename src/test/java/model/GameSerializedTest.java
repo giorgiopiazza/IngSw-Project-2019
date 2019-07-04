40d8c9d672ac591d5524e9d3a6323d6939ba0fdd
tosettil <lorenzo.tosetti@mail.polimi.it>
2019-07-04 02:41:37 +0200

    Tests and code smells fixes

--- src/test/java/model/GameSerializedTest.java
+++ src/test/java/model/GameSerializedTest.java
@@ -1,14 +1,22 @@
 package model;
 
 import enumerations.PlayerColor;
+import enumerations.PossibleAction;
 import exceptions.game.InvalidMapNumberException;
+import exceptions.player.MaxCardsInHandException;
+import model.cards.PowerupCard;
+import model.cards.WeaponCard;
 import model.map.GameMap;
 import model.player.Bot;
 import model.player.PlayerBoard;
 import model.player.UserPlayer;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import utility.PowerupParser;
+import utility.WeaponParser;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -19,23 +27,29 @@ class GameSerializedTest {
     Game game;
 
     @BeforeEach
-    void before() throws InvalidMapNumberException {
+    void before() {
         game = Game.getInstance();
-        game.init();
-        game.initializeDecks();
-        game.setGameMap(GameMap.MAP_3);
     }
 
     @Test
-    void noBot() {
+    void noBot() throws InvalidMapNumberException, MaxCardsInHandException {
+        game.init();
+        game.initializeDecks();
+        game.setGameMap(GameMap.MAP_3);
+
         UserPlayer p1 = new UserPlayer("1");
+        p1.setWeapons(new ArrayList<>(List.of((WeaponCard) WeaponParser.parseCards().draw())));
         UserPlayer p2 = new UserPlayer("2");
+        p2.setWeapons(new ArrayList<>(List.of((WeaponCard) WeaponParser.parseCards().draw())));
         game.addPlayer(p1);
         game.addPlayer(p2);
         game.addPlayer(new UserPlayer("asd"));
 
         GameSerialized gameSerialized = new GameSerialized("1");
 
+        gameSerialized.getPlayerWeapons("1");
+        gameSerialized.getPlayerWeapons("2");
+        gameSerialized.getPlayerWeapons("3");
         gameSerialized.setPlayers(List.of(mock(UserPlayer.class)));
         gameSerialized.setPlayers(null);
         gameSerialized.getPlayers();
@@ -55,7 +69,27 @@ class GameSerializedTest {
     }
 
     @Test
-    void bot() {
+    void bot() throws InvalidMapNumberException {
+        game.init();
+        game.initializeDecks();
+        game.setGameMap(GameMap.MAP_3);
 
+        UserPlayer p1 = new UserPlayer("1");
+        p1.setPossibleActions(new HashSet<>(List.of(PossibleAction.BOT_ACTION)));
+        p1.setSpawningCard((PowerupCard) PowerupParser.parseCards().draw());
+        UserPlayer p2 = new UserPlayer("2");
+        game.addPlayer(p1);
+        game.addPlayer(p2);
+        game.addPlayer(new UserPlayer("asd"));
+        game.setTerminator(true);
+        game.buildTerminator();
+
+        GameSerialized gameSerialized = new GameSerialized("1");
+        gameSerialized.getBot();
+        gameSerialized.getPowerupCards();
+        gameSerialized.getPowerupCards();
+        gameSerialized.isBotActionDone();
+        gameSerialized.getAllPlayers();
+        assertTrue(gameSerialized.isBotPresent());
     }
 }
