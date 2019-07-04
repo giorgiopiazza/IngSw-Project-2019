10ec9b870e94f94e8df279857fe8087bade23fad
tosettil <lorenzo.tosetti@mail.polimi.it>
2019-07-05 01:55:18 +0200

    fix

--- src/test/java/model/actions/MoveActionTest.java
+++ src/test/java/model/actions/MoveActionTest.java
@@ -11,7 +11,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-public class MoveActionTest {
+class MoveActionTest {
     @Test
     void moveAction() throws Exception {
         UserPlayer moving = new UserPlayer("1", PlayerColor.GREEN, new PlayerBoard());
